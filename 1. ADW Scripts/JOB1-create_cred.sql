9a2a9e7a6677e861b884e790f48243fa6ea5bf1b
alexblyth <85843+alexblyth@users.noreply.github.com>
2019-11-07 10:41:47 +1100

    Update to use new file location

--- 1. ADW Scripts/JOB1-create_cred.sql
+++ 1. ADW Scripts/JOB1-create_cred.sql
@@ -9,7 +9,7 @@ begin
 DBMS_CLOUD.create_credential( 
     credential_name => 'OBJ_STORE_CRED', 
     username => 'dataload',
-    password => 'kHgO<WLw6Pfz0R1:Pvx1'
+    password => 'O78;7yiu37BmO3{l8M>D'
     ); 
 end;
 /
@@ -24,6 +24,6 @@ FROM table(dbms_cloud.list_objects(
 SELECT * 
 FROM table(dbms_cloud.list_objects(
              credential_name => 'OBJ_STORE_CRED', 
-             location_uri => 'https://objectstorage.us-ashburn-1.oraclecloud.com/n/idzo73zlod7f/b/DATA-PUMP/o/'
+             location_uri => 'https://objectstorage.ap-sydney-1.oraclecloud.com/n/sdwrxspaxozk/b/adwroadshow/o/'
            ));
 /
